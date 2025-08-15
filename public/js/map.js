10b8016c0393665382acd2ee6dff8a087f383444
Vikas Yadav <vikasyadavmvm7897@gmail.com>
2025-08-15 09:02:54 +0530

    correction to show map

--- public/js/map.js
+++ public/js/map.js
@@ -1,20 +1,20 @@
-maptilersdk.config.apiKey = mapToken;
+maptilersdk.config.apiKey = window.mapToken;
 
 const map = new maptilersdk.Map({
-  container: 'map',
-  style: maptilersdk.MapStyle.STREETS,
-  center: coordinates,
-  zoom: 6
+    container: 'map',
+    style: maptilersdk.MapStyle.STREETS,
+    center: window.coordinates,
+    zoom: 6
 });
 
 const popup = new maptilersdk.Popup({ offset: 25 })
-  .setLngLat(coordinates)
-  .setHTML(`
-    <h6>${listingData.title}</h6>
-    <p>Exact location will be provided after booking.</p>
-  `);
+    .setLngLat(window.coordinates)
+    .setHTML(`
+        <h6>${window.listingData.title}</h6>
+        <p>Exact location will be provided after booking.</p>
+    `);
 
 new maptilersdk.Marker({ color: 'red' })
-  .setLngLat(coordinates)
-  .setPopup(popup)
-  .addTo(map);
+    .setLngLat(window.coordinates)
+    .setPopup(popup)
+    .addTo(map);
